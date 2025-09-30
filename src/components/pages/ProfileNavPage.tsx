a9deb475d327b71c8b042b21b6df0d5c5a978469
moizhassan7 <hassanmoiz.119sb@gmail.com>
2025-09-30 23:38:43 +0500

    new comit

--- src/components/pages/ProfileNavPage.tsx
+++ src/components/pages/ProfileNavPage.tsx
@@ -309,7 +309,8 @@ const ProfileNavPage: React.FC<ProfileNavPageProps> = ({ user, setCurrentPage, h
               لاگ آؤٹ
             </button>
             <p className="text-xs text-gray-400" dir="ltr">
-              Developed by Moiz Hassan
+              Developed by Moiz Hassan. <br/>
+              If you are facing any issue you can tell me directly, by sending whatsapp message on 03184902119
             </p>
           </div>
         </div>
